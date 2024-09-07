8d532cc50b46521adeeb06044729ee941ae7339e
sem-a <aleksey.i99@mail.ru>
2024-09-07 16:18:57 +0300

    убран фетч запрос

--- src/components/movie-list/index.tsx
+++ src/components/movie-list/index.tsx
@@ -1,40 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { setMovies } from "../../store/moviesSlice";
-import { RootState, AppDispatch } from "../../store/store";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import Movie from "../movie";
 import styles from "./index.module.scss";
-import { API_FETCH, API_KEY } from "../../const";
 import { MovieType } from "../../types";
 
 const MovieList: React.FC = () => {
     const movies = useSelector((state: RootState) => state.movies.movies);
-    const dispatch = useDispatch<AppDispatch>();
     const [visibleMovies, setVisibleMovies] = useState(6);
     const [filter, setFilter] = useState("all");
     const [activeFilter, setActiveFilter] = useState("all");
     const [sortByYear, setSortByYear] = useState(false);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(API_FETCH, {
-                    headers: {
-                        "X-API-KEY": API_KEY,
-                        accept: "application/json",
-                    },
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    dispatch(setMovies(data.docs));
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        fetchData();
-    }, [dispatch]);
 
     const showMoreMovies = () => {
         setVisibleMovies((prevVisibleMovies) => prevVisibleMovies + 6);
